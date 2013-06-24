9be1f51120e2421dd2631be364a02a987484615f
Marlon Baeten <marlon@baeten.org>
2013-06-24 15:02:01 +0200

    Initial commit

---
+++ BsBootstrapifyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bs\BootstrapifyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BsBootstrapifyBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Bravesheep Bootstrapify bundle
